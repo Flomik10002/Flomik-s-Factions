fac02581960d821e398eb9ef05fb61eefa443d94
Flomik <79540285+Flomik10002@users.noreply.github.com>
2024-12-09 14:13:02 +0400

    Initial commit

---
+++ src/main/java/org/flomik/flomiksFactions/worldEvents/randomEvents/RandomEvent.java
@@ -0,0 +1,81 @@
+package org.flomik.flomiksFactions.worldEvents.randomEvents;
+
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.boss.BossBar;
+import org.bukkit.entity.Player;
+import org.flomik.flomiksFactions.FlomiksFactions;
+
+import java.util.*;
+
+public abstract class RandomEvent {
+    protected final FlomiksFactions plugin;
+    protected final int duration;
+    protected final List<Integer> rewards;
+    protected final Map<Player, Integer> scores;
+    protected BossBar bossBar;
+    protected boolean running;
+    private boolean finished;
+
+    public RandomEvent(FlomiksFactions plugin, int duration, List<Integer> rewards) {
+        this.plugin = plugin;
+        this.duration = duration;
+        this.rewards = rewards;
+        this.scores = new HashMap<>();
+        this.finished = false;
+    }
+
+    public abstract void start();
+    public abstract void stop();
+    public abstract void onTick();
+
+    public boolean isRunning() {
+        return running;
+    }
+
+    public BossBar getBossBar() {
+        return bossBar;
+    }
+
+    protected void updateBossBarProgress(int remainingTime, String EventName) {
+        double progress = (double) remainingTime / duration;
+        bossBar.setProgress(Math.max(0, progress));
+        bossBar.setTitle("§a[Ивент] §6" + EventName + ". Оставшееся время: " + remainingTime + " секунд");
+    }
+
+    protected void announceEventStart(String eventName, String description) {
+        Bukkit.broadcastMessage("§a[Ивенты] §fИвент §6" + eventName + " §fначался! Участвуйте и побеждайте!\n§e" + description);
+    }
+
+    protected void announceResults(String eventName, String achievementAction, String achievementUnit) {
+        if (finished) {
+            plugin.getLogger().info("Результаты уже были объявлены.");
+            return;
+        }
+
+        finished = true;
+
+        List<Map.Entry<Player, Integer>> sortedScores = scores.entrySet().stream()
+                .sorted(Map.Entry.<Player, Integer>comparingByValue().reversed())
+                .toList();
+
+        if (sortedScores.isEmpty()) {
+            Bukkit.broadcastMessage("§a[Ивенты] §fИвент §6" + eventName + " §fзавершён, но никто не принял участие.");
+            return;
+        }
+
+        Bukkit.broadcastMessage("§a[Ивенты] §fИвент §6" + eventName + " §fзавершён! Результаты:");
+
+        for (int i = 0; i < rewards.size() && i < sortedScores.size(); i++) {
+            Player player = sortedScores.get(i).getKey();
+            int achievement = sortedScores.get(i).getValue();
+            int reward = rewards.get(i);
+
+            player.sendMessage(ChatColor.GOLD + "Вы заняли " + (i + 1) + " место и получили " + reward + " монет!");
+
+            plugin.getEconomy().depositPlayer(player, reward);
+
+            Bukkit.broadcastMessage("§6" + (i + 1) + " место: §e" + player.getName() + " §f— " + achievementAction + " " + achievement + " " + achievementUnit + " §fи получил §6" + reward + " золота!");
+        }
+    }
+}
---
+++ src/main/java/org/flomik/flomiksFactions/worldEvents/randomEvents/RandomEventManager.java
@@ -0,0 +1,52 @@
+package org.flomik.flomiksFactions.worldEvents.randomEvents;
+
+import org.flomik.flomiksFactions.FlomiksFactions;
+import org.flomik.flomiksFactions.worldEvents.randomEvents.events.DiverEvent;
+import org.flomik.flomiksFactions.worldEvents.randomEvents.events.FishermanEvent;
+import org.flomik.flomiksFactions.worldEvents.randomEvents.events.RunnerEvent;
+
+public class RandomEventManager {
+    private final FlomiksFactions plugin;
+    private RandomEvent currentEvent;
+
+    public RandomEventManager(FlomiksFactions plugin) {
+        this.plugin = plugin;
+    }
+
+    public boolean isRunning() {
+        return currentEvent != null && currentEvent.isRunning();
+    }
+
+    public void startEvent(String eventType) {
+        if (isRunning()) {
+            plugin.getLogger().info("Ивент уже запущен.");
+            return;
+        }
+
+        switch (eventType.toLowerCase()) {
+            case "fisherman" -> currentEvent = new FishermanEvent(plugin);
+            case "diver" -> currentEvent = new DiverEvent(plugin);
+            case "runner" -> currentEvent = new RunnerEvent(plugin);
+            default -> {
+                plugin.getLogger().info("Неизвестный тип ивента: " + eventType);
+                return;
+            }
+        }
+
+
+        currentEvent.start();
+        plugin.getLogger().info(eventType + " ивент запущен!");
+    }
+
+    public RandomEvent getCurrentEvent() {
+        return currentEvent;
+    }
+
+    public void stopEvent() {
+        if (currentEvent != null) {
+            currentEvent.stop();
+            currentEvent = null;
+            plugin.getLogger().info("Ивент остановлен.");
+        }
+    }
+}
